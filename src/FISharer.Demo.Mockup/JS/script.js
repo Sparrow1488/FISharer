e9966b0f3cb8ad9e5dff47bfb0dc3c2540291e36
Валентин <ilyaokubev@gmail.com>
2021-10-28 13:43:24 +0300

    FS: починил мягкий скролл

--- src/FISharer.Demo.Mockup/JS/script.js
+++ src/FISharer.Demo.Mockup/JS/script.js
@@ -1,3 +1,7 @@
+const mainBlocks = document.querySelectorAll(".main__item");
+const navBtns = document.querySelectorAll(".nav__btn");
+const activeBtnColors = ["#C70039", "#FFC300", "#1A5276", "#F7DC6F", "#F4F6F7"];
+
 function hidePreloader(){
     $(".preloader").animate({ opacity: 0 }, "slow");
     setTimeout(() => {
@@ -10,38 +14,43 @@ function enableScroll(){
 function disableScroll(){
     $("body").css("overflow-y", "hidden");
 }
-function init(){
+function siteStartPosition(){
     $(window).scrollTop(0);
 }
-function hideAll(){
-    $(".main__item").hide();
+function setNavBtnActive(button){
+    const rndActiveColor = activeBtnColors[Math.floor(Math.random() * activeBtnColors.length)];
+
+    $(navBtns).removeClass("nav__btn_active");
+    $(button).addClass("nav__btn_active");
+    $(navBtns).css("background", "transparent");
+    $(button).css("background", rndActiveColor);
 }
-function showBlock(index){
-    hideAll();
-    const first = document.querySelectorAll(".main__item")[index];
-    $(first).show();
+function setNavBtnActiveByIndex(index){
+    setNavBtnActive(navBtns[index]);
 }
 
 $(document).ready(function () {
-    init();
-    // showBlock(0);
-    disableScroll();
+    siteStartPosition();
     setTimeout(() => {
         hidePreloader();
-        enableScroll();
     }, 1500);
+
+    $(".up-btn").click(function(){
+        setNavBtnActiveByIndex(0);
+        window.scroll({top: 0, left: 0, behavior: 'smooth' });
+    });
 
     $(".burger").click(function() {
         $(".menu").toggle("slow");
     });
 
-    const navBtns = document.querySelectorAll(".nav__btn");
-    const mainBlocks = document.querySelectorAll(".main__item");
     $(navBtns).click(function(e) {
         e.preventDefault();
+
         const btnIndex = $(this).index();
         const positY = $(mainBlocks[btnIndex]).offset().top;
-        console.log(positY);
+        setNavBtnActive(this);
+
         window.scroll({top: positY, left: 0, behavior: 'smooth' });
     });
     
