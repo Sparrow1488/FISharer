d8d1b0a2e59c7deb143a60717c27424d9e360354
Валентин <ilyaokubev@gmail.com>
2021-10-28 13:09:21 +0300

    FS: добавил плавный скролл на главную страницу

--- src/FISharer.Demo.Mockup/JS/script.js
+++ src/FISharer.Demo.Mockup/JS/script.js
@@ -4,14 +4,45 @@ function hidePreloader(){
         $(".preloader").remove();
     }, 500);
 }
+function enableScroll(){
+    $("body").css("overflow-y", "scroll");
+}
+function disableScroll(){
+    $("body").css("overflow-y", "hidden");
+}
+function init(){
+    $(window).scrollTop(0);
+}
+function hideAll(){
+    $(".main__item").hide();
+}
+function showBlock(index){
+    hideAll();
+    const first = document.querySelectorAll(".main__item")[index];
+    $(first).show();
+}
 
 $(document).ready(function () {
+    init();
+    // showBlock(0);
+    disableScroll();
     setTimeout(() => {
         hidePreloader();
+        enableScroll();
     }, 1500);
 
-    // $(".menu").show();
     $(".burger").click(function() {
         $(".menu").toggle("slow");
     });
+
+    const navBtns = document.querySelectorAll(".nav__btn");
+    const mainBlocks = document.querySelectorAll(".main__item");
+    $(navBtns).click(function(e) {
+        e.preventDefault();
+        const btnIndex = $(this).index();
+        const positY = $(mainBlocks[btnIndex]).offset().top;
+        console.log(positY);
+        window.scroll({top: positY, left: 0, behavior: 'smooth' });
+    });
+    
 });
