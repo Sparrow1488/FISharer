742fe28bf4d723e529335217c2804f15680bd8ae
Sparrow1488 <ilyaokubev@gmail.com>
2021-11-19 21:51:01 +0300

    FS: добавил лимиты и ограничения для моделей данных

--- src/FISharer/FISharer/Entities/DataInfo.cs
+++ src/FISharer/FISharer/Entities/DataInfo.cs
@@ -6,7 +6,10 @@ namespace FISharer.Entities
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(128)]
         public string Name { get; set; }
+        [Range(1, long.MaxValue)]
         public long Size { get; set; }
         public ClientData ArchiveData { get; set; }
     }
