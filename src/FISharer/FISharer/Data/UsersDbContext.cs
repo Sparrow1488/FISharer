b6bec31fa40a7c8ce32537763f4909c3133ea154
Sparrow1488 <ilyaokubev@gmail.com>
2021-12-10 09:51:47 +0300

    FS: внедрил авторизацию (пока не тестил)

---
+++ src/FISharer/FISharer/Data/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FISharer.Entities.Clients;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FISharer.Data
+{
+    public class UsersDbContext : IdentityDbContext<User, UserRole, Guid>
+    {
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ src/FISharer/FISharer/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FISharer.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
